78b777dd1d6b72b40faf33a69471d4008e85c05a
khrixtar-dev <khrixtar.gg@gmail.com>
2025-08-27 11:17:54 -0400

    Creacion de calculadora trucha2

--- app/src/main/java/com/example/proyectobase/MainActivity4.kt
+++ app/src/main/java/com/example/proyectobase/MainActivity4.kt
@@ -3,11 +3,15 @@ package com.example.proyectobase
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+//Import del package custom llamado utils
+import com.example.proyectobase.utils.OpMatematicas
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +20,20 @@ class MainActivity4 : AppCompatActivity() {
         setContentView(R.layout.activity_main4)
 
         val txTituloCalculadora : TextView = findViewById(R.id.txTituloCalculadora)
-        var edNumero1 : EditText = findViewById(R.id.edNumero1)
-        var edNumero2 : EditText = findViewById(R.id.edNumero2)
-        var txTextView : TextView =findViewById(R.id.txTextView)
-
+        val edNumero1 : EditText = findViewById(R.id.edNumero1)
+        val edNumero2 : EditText = findViewById(R.id.edNumero2)
+        val txResultado : TextView =findViewById(R.id.txResultado)
+        var spSeleccionarOperacion : Spinner = findViewById(R.id.spSeleccionarOperacion)
         val btnCalcular : Button = findViewById(R.id.btnCalcular)
 
+        btnCalcular.setOnClickListener {
+            var calcN1 : Int = edNumero1.text.toString().toIntOrNull()?:0
+            var calcN2 : Int = edNumero2.text.toString().toIntOrNull()?:0
+
+            var resultadoOP : Int = OpMatematicas.sumar(calcN1,calcN2)
+
+            txResultado.text = resultadoOP.toString()
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
---
+++ app/src/main/java/com/example/proyectobase/utils/OpMatematicas.kt
@@ -0,0 +1,22 @@
+package com.example.proyectobase.utils
+
+object OpMatematicas {
+    // En python es def, aqui es fun, y al ingresarle variables n1 y n2 hay que definirles el
+    // tipo de dato y el Int que va solo es el tipo de dato del resultado
+
+    fun sumar(n1:Int, n2:Int) : Int{
+        return n1 + n2
+    }
+
+    fun restar(n1:Int, n2:Int) : Int{
+        return n1 - n2
+    }
+
+    fun multiplicar(n1:Int, n2:Int) : Int{
+        return n1 * n2
+    }
+
+    fun dividir(n1:Int, n2:Int) : Int{
+        return n1 / n2
+    }
+}
